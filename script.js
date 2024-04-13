ab5705328b14d9100030e7c7dd8c19c3dcbd2c9a
Vishal <vp7390734@gmail.com>
2024-04-13 09:02:10 +0530

    fix the shery.js gooey animation for mobile device

--- script.js
+++ script.js
@@ -148,15 +148,29 @@ function cursorAnimation() {
     })
 }
 function sheryAnimation() {
-    Shery.imageEffect(".img-items", {
-        style: 5,
-        config: { "a": { "value": 2.52, "range": [0, 30] }, "b": { "value": 0.76, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749932567818 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.61, "range": [0, 10] }, "metaball": { "value": 0.49, "range": [0, 2] }, "discard_threshold": { "value": 0.74, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.43, "range": [0, 2] }, "noise_scale": { "value": 9.16, "range": [0, 100] } },
-        gooey: true
-    })
-
+    // It check the device width for shery js animation if it is >=768px then it will call the gooeyAnimation fun otherwise not.....
+    function checkScreenWidth() {
+        var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (screenWidth >= 768) {
+            gooeyAnimation();
+        }
+    }
+    function gooeyAnimation() {
+        Shery.imageEffect(".img-items", {
+            style: 5,
+            config: { "a": { "value": 2.52, "range": [0, 30] }, "b": { "value": 0.76, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749932567818 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.61, "range": [0, 10] }, "metaball": { "value": 0.49, "range": [0, 2] }, "discard_threshold": { "value": 0.74, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.43, "range": [0, 2] }, "noise_scale": { "value": 9.16, "range": [0, 100] } },
+            gooey: true
+        })
+    }
+    // Calling checkScreenWidth fun with eventlistener on "load" and "resize" of browser....
+    window.addEventListener('load', checkScreenWidth);
+    window.addEventListener('resize', checkScreenWidth);
 }
+
 locoMotive()
 loaderAnimation()
 flagAnimation()
 cursorAnimation()
 sheryAnimation()
+
+
